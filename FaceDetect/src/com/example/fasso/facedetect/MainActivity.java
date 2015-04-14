3692fc730eebe700f5c24e31eca91f2e71ff27c2
Lokesh <tiwari9773@gmail.com>
2015-04-14 15:38:26 +0530

    Check in Other file

--- FaceDetect/src/com/example/fasso/facedetect/MainActivity.java
+++ FaceDetect/src/com/example/fasso/facedetect/MainActivity.java
@@ -1,26 +1,180 @@
 package com.example.fasso.facedetect;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
-import android.graphics.drawable.Drawable;
 import android.media.FaceDetector;
 import android.media.FaceDetector.Face;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+
+	Bitmap myBitmap;
+	private static int height;
+	private static int width;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
-		setContentView(new myView(this));
+		setContentView(R.layout.activity_main);
+
+		init();
+	}
+
+	public void init() {
+
+		/** Calculate device Height and width */
+		DisplayMetrics metrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+		height = metrics.heightPixels;
+		width = metrics.widthPixels;
+
+		/** Display just for Visibility */
+		Toast.makeText(this, "Height: " + height + " " + "Width: " + width, Toast.LENGTH_LONG).show();
+
+		/** Initialize with click event */
+		Button btnAdd = (Button) findViewById(R.id.btn_add);
+		btnAdd.setOnClickListener(this);
+
+		Button btnGalary = (Button) findViewById(R.id.btn_galary);
+		btnGalary.setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent intent;
+		switch (v.getId()) {
+		case R.id.btn_add:
+			intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+			startActivityForResult(intent, 1);
+			break;
+
+		case R.id.btn_galary:
+			intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+			startActivityForResult(intent, 2);
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == RESULT_OK) {
+			if (requestCode == 1) {
+				File f = new File(Environment.getExternalStorageDirectory().toString());
+				for (File temp : f.listFiles()) {
+					if (temp.getName().equals("temp.jpg")) {
+						f = temp;
+						break;
+					}
+				}
+				try {
+					BitmapFactory.Options BitmapFactoryOptionsbfo = new BitmapFactory.Options();
+					BitmapFactoryOptionsbfo.inPreferredConfig = Bitmap.Config.RGB_565;
+
+					myBitmap = BitmapFactory.decodeFile(f.getAbsolutePath(), BitmapFactoryOptionsbfo);
+
+					/**
+					 * Width would be same as device but height of image may vary as per ratio of
+					 */
+					myBitmap = Bitmap.createScaledBitmap(myBitmap, width, calculateNewHeight(myBitmap), true);
+
+					View v = new myView(this);
+					LinearLayout llImage = (LinearLayout) findViewById(R.id.ll_add_image);
+					llImage.removeAllViews();
+					llImage.addView(v);
+
+					// String path = android.os.Environment.getExternalStorageDirectory() + File.separator + "Download";
+					// f.delete();
+					//
+					// OutputStream outFile = null;
+					// File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+					// try {
+					// outFile = new FileOutputStream(file);
+					// myBitmap.compress(Bitmap.CompressFormat.JPEG, 40, outFile);
+					//
+					//
+					// outFile.flush();
+					// outFile.close();
+					//
+					//
+					// } catch (FileNotFoundException e) {
+					// e.printStackTrace();
+					// } catch (IOException e) {
+					// e.printStackTrace();
+					// } catch (Exception e) {
+					// e.printStackTrace();
+					// }
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else if (requestCode == 2) {
+
+				Uri selectedImage = data.getData();
+				String[] filePath = { MediaStore.Images.Media.DATA };
+
+				Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+				c.moveToFirst();
+				int columnIndex = c.getColumnIndex(filePath[0]);
+				String picturePath = c.getString(columnIndex);
+				c.close();
+
+				/** Initialised BitMap Images */
+				BitmapFactory.Options BitmapFactoryOptionsbfo = new BitmapFactory.Options();
+				BitmapFactoryOptionsbfo.inPreferredConfig = Bitmap.Config.RGB_565;
+
+				myBitmap = BitmapFactory.decodeFile(picturePath, BitmapFactoryOptionsbfo);
+				/**
+				 * Width would be same as device but height of image may vary as per ratio of
+				 */
+				myBitmap = Bitmap.createScaledBitmap(myBitmap, width, calculateNewHeight(myBitmap), true);
+
+				Log.w("path of image from gallery......******************.........", picturePath + "");
+				View v = new myView(this);
+				LinearLayout llImage = (LinearLayout) findViewById(R.id.ll_add_image);
+				llImage.removeAllViews();
+				llImage.addView(v);
+			}
+		}
+	}
+
+	/** height of image may vary as per ratio of Image with respect to width */
+	public static int calculateNewHeight(Bitmap myBitmap) {
+		float tmp = ((float) myBitmap.getHeight() / (float) myBitmap.getWidth());
+		float newHeight = tmp * width;
+		return (int) newHeight;
 	}
 
 	private class myView extends View {
@@ -33,24 +187,24 @@ public class MainActivity extends Activity {
 		float myEyesDistance;
 		int numberOfFaceDetected;
 
-		Bitmap myBitmap;
-
 		public myView(Context context) {
 
 			super(context);
 
-			// this.scrollBy(100, 100);
 			BitmapFactory.Options BitmapFactoryOptionsbfo = new BitmapFactory.Options();
 			BitmapFactoryOptionsbfo.inPreferredConfig = Bitmap.Config.RGB_565;
 
-			myBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.face_detection, BitmapFactoryOptionsbfo);
+			// myBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.face_detection,BitmapFactoryOptionsbfo);
+			if (myBitmap != null) {
+				imageWidth = myBitmap.getWidth();
+				imageHeight = myBitmap.getHeight();
 
-			imageWidth = myBitmap.getWidth();
-			imageHeight = myBitmap.getHeight();
+				myFace = new FaceDetector.Face[numberOfFace];
+				myFaceDetect = new FaceDetector(imageWidth, imageHeight, numberOfFace);
+				numberOfFaceDetected = myFaceDetect.findFaces(myBitmap, myFace);
 
-			myFace = new FaceDetector.Face[numberOfFace];
-			myFaceDetect = new FaceDetector(imageWidth, imageHeight, numberOfFace);
-			numberOfFaceDetected = myFaceDetect.findFaces(myBitmap, myFace);
+				Toast.makeText(MainActivity.this, "FaceCount=" + numberOfFaceDetected, Toast.LENGTH_LONG).show();
+			}
 		}
 
 		@Override
@@ -76,4 +230,5 @@ public class MainActivity extends Activity {
 		}
 
 	}
+
 }
